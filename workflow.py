ecf1b1b81df5e462eea7ad57f8921a51baa2c961
andynet <andrejbalaz001@gmail.com>
2018-09-21 14:16:50 +0200

    added targets create_vcf and filter_noncancer_variant

--- workflow.py
+++ workflow.py
@@ -2,6 +2,24 @@ from gwf import Workflow
 
 gwf = Workflow()
 
-gwf.target('MyTarget', inputs=[], outputs=['greeting.txt']) << """
-echo hello world > greeting.txt
-"""
+ref_genome = 'hg38.fa'
+samples_bam = ['sample_cancer_filter_sort.bam', 'sample_blood_filter_sort.bam']
+samples_vcf = ['sample_cancer.vcf', 'sample_blood.vcf']
+filtered_vcf = 'filtered.vcf'
+
+for i in range(len(samples_bam)):
+    gwf.target(
+        'create_vcf',
+        inputs=[f'{ref_genome}', f'{samples_bam[i]}'],
+        outputs=[f'{samples_vcf[i]}']) \
+        << f"""    
+        samtools mpileup -u -f {ref_genome} {samples_bam[i]} | bcftools view -v -c - > {samples_vcf[i]}
+        """
+
+gwf.target(
+    'filter_noncancer_variants',
+    inputs=[f'{samples_vcf[0]}', f'{samples_vcf[1]}'],
+    outputs=[f'{filtered_vcf}']) \
+    << f"""
+    vcftools {samples_vcf[1]} --diff {samples_vcf[0]} --diff-site --out {filtered_vcf}       
+    """
