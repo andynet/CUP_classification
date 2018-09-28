77ff1aba1030d8fa349a57be84650a1ba3aebfa5
andynet <andrejbalaz001@gmail.com>
2018-09-28 15:37:49 +0200

    bugfix

--- workflow.py
+++ workflow.py
@@ -2,7 +2,7 @@ from gwf import Workflow
 
 gwf = Workflow()
 
-working_directory = '/faststorage/project/CUP_classification/faststorage/Andrej'
+working_directory = '/faststorage/project/CUP_classification/Andrej'
 
 reference_genome = f'{working_directory}/hg38.fa'
 bowtie2_index = f'{working_directory}/bowtie2_index/hg38'
